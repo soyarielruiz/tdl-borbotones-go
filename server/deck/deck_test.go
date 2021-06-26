7f935e28d8e5efe35958b1582c4f44fc8bf433b8
Luis Tejerina <luist@paypertic.com>
2021-06-26 18:24:01 -0300

    Tests

--- server/deck/deck_test.go
+++ server/deck/deck_test.go
@@ -23,8 +23,34 @@ func TestDeck_GetCardsFromDeck(t *testing.T) {
 	}
 }
 
+func TestDeck_GetFrontCard(t *testing.T) {
+	deck := NewDeck()
+	card := deck.GetCardFromDeck()
+	if !isValidCard(card) {
+		t.Errorf("Invalid card got")
+	}
+}
+
+func TestDeck_GetFrontCardTwice(t *testing.T) {
+	deck := NewDeck()
+	card := deck.GetFrontCard()
+	card2 := deck.GetFrontCard()
+	if card != card2 {
+		t.Errorf("Invalid card got")
+	}
+}
+
+func TestDeck_PutCard(t *testing.T) {
+	deck := NewDeck()
+	card := deck.GetFrontCard()
+	sentCard := tools.Card{1, tools.GREEN}
+	deck.PutCard(sentCard)
+	card2 := deck.GetFrontCard()
+	if card == card2 || card2 != sentCard {
+		t.Errorf("Invalid card got")
+	}
+}
 
 func isValidCard(card tools.Card) bool {
 	return card.Number >= 0 && card.Number <= 9
 }
-
